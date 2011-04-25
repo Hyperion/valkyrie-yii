fc5e04a8bf8d03126a10878451b896978372d5d1
Artem Korneev <hyperion1488@gmail.com>
2011-04-25 14:11:50 +0400

    Исправлена форма регистрации аккаунта

--- protected/modules/wow/views/cp/_register.php
+++ protected/modules/wow/views/cp/_register.php
@@ -11,14 +11,20 @@
 
     <div class="row">
         <?php echo $form->labelEx($model,'username'); ?>
-        <?php echo $form->passwordField($model,'username'); ?>
+        <?php echo $form->textField($model,'username'); ?>
         <?php echo $form->error($model,'username'); ?>
+    </div>
+	
+    <div class="row">
+        <?php echo $form->labelEx($model,'password'); ?>
+        <?php echo $form->passwordField($model,'password'); ?>
+        <?php echo $form->error($model,'password'); ?>
     </div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
+		<?php echo $form->labelEx($model,'verifyPassword'); ?>
+		<?php echo $form->passwordField($model,'verifyPassword'); ?>
+		<?php echo $form->error($model,'verifyPassword'); ?>
 	</div>
 
     <div class="row">
