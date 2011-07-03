1fc48ab81b74c686164d5da70f29633b82e73b53
Artem Korneev <hyperion1488@gmail.com>
2011-07-03 15:28:19 +0400

    Внешний вид админки

--- protected/modules/wow/views/backend/character/_search.php
+++ protected/modules/wow/views/backend/character/_search.php
@@ -1,18 +1,18 @@
 <div class="wide form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
-	'action'=>Yii::app()->createUrl($this->route),
-	'method'=>'get',
+    'action'=>Yii::app()->createUrl($this->route),
+    'method'=>'get',
 )); ?>
 
-	<div class="row">
-		<?php echo $form->label($model,'name'); ?>
-		<?php echo $form->textField($model,'name',array('size'=>12,'maxlength'=>12)); ?>
-	</div>
+    <div class="row">
+        <?php echo $form->label($model,'name'); ?>
+        <?php echo $form->textField($model,'name',array('class' => 'text-input medium-input')); ?>
+    </div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
+    <div class="row">
+        <?php echo CHtml::submitButton('Search', array('class' => 'button')); ?>
+    </div>
 
 <?php $this->endWidget(); ?>
 
