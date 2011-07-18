c691c26c5e890e8cff062f4f1d25c439d453d7b7
gabriel@gabriel-MS-N014 <gabriel@gabriel-MS-N014>
2011-07-19 03:56:41 +0400

    Netbeans test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../src/yii/framework/yii.php';
+$yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/frontend.php';
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
