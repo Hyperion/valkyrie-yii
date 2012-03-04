4d0ae14b6965964eccb13bcced94a2080b99e79b
Artem Korneev <hyperion1488@gmail.com>
2012-03-04 21:50:48 +0200

    Убраны доп поля меню в админке

--- protected/modules/rights/views/backend/assignment/view.php
+++ protected/modules/rights/views/backend/assignment/view.php
@@ -3,33 +3,6 @@ $this->breadcrumbs = array(
     'Rights' => Rights::getBaseUrl(),
     Rights::t('core', 'Assignments'),
 );
-$this->menu = array_merge($this->menu, array(
-    array(
-        'label' => Rights::t('core', 'Assignments'),
-        'url'   => array('assignment/view'),
-        'itemOptions' => array('class' => 'item-assignments'),
-    ),
-    array(
-        'label' => Rights::t('core', 'Permissions'),
-        'url'   => array('authItem/permissions'),
-        'itemOptions' => array('class' => 'item-permissions'),
-    ),
-    array(
-        'label' => Rights::t('core', 'Roles'),
-        'url'   => array('authItem/roles'),
-        'itemOptions' => array('class' => 'item-roles'),
-    ),
-    array(
-        'label' => Rights::t('core', 'Tasks'),
-        'url'   => array('authItem/tasks'),
-        'itemOptions' => array('class' => 'item-tasks'),
-    ),
-    array(
-        'label' => Rights::t('core', 'Operations'),
-        'url'   => array('authItem/operations'),
-        'itemOptions' => array('class' => 'item-operations'),
-    ),
-        ));
 ?>
 
 <h2><?php echo Rights::t('core', 'Assignments'); ?></h2>
